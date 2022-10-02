f15ab3b8ac68225072f3e6ab9d57ea88698a6dcf
Fabio Yamashita <yamashitafabio@gmail.com>
2022-10-01 21:26:25 -0300

    feat: Add PlayerServiceAutoMockerFixture and PlayerServiceTestsFixture

---
+++ Exercício 2 - Mock/MockingUnitTestsDemoApp.Tests/Services_AutoMockerFixture/PlayerServiceAutoMockerFixtureTests.cs
@@ -0,0 +1,125 @@
+﻿using FluentAssertions;
+using MockingUnitTestsDemoApp.Impl.Models;
+using MockingUnitTestsDemoApp.Impl.Repositories.Interfaces;
+using MockingUnitTestsDemoApp.Impl.Services;
+using Moq;
+using Xunit;
+
+namespace MockingUnitTestsDemoApp.Tests.Services_AutoMockerFixture
+{
+    [Collection(nameof(PlayerServiceCollection))]
+    public class PlayerServiceAutoMockerFixtureTests
+    {
+        private readonly PlayerService _subject;
+        private readonly PlayerServiceTestsFixture _playerServiceTestsFixture;
+
+        public PlayerServiceAutoMockerFixtureTests(PlayerServiceTestsFixture playerServiceTestsFixture)
+        {
+            _playerServiceTestsFixture = playerServiceTestsFixture;
+            _subject = _playerServiceTestsFixture.CreatePlayerService();
+        }
+
+        #region GetByID TESTS 
+        // USING AUTOFIXTURE TO CREATE A FAKE PLAYER OBJECT
+        [Fact(DisplayName = "GetByID Method when ID is valid")]
+        [Trait("Ex.2", "PlayerServiceAutoMockerFixture Tests")]
+        public void GetByID_IdIsValid_ShouldReturnPlayer()
+        {
+            // Arrange
+            Player player = _playerServiceTestsFixture.CreatePlayerUsingAutoFixture();
+
+            _playerServiceTestsFixture.Mocker.GetMock<IPlayerRepository>()
+                .Setup(pr => pr.GetByID(player.ID))
+                .Returns(player);
+
+            // Act
+            var actual = _subject.GetByID(player.ID);
+
+            // Assert
+            actual.Should()
+                .BeEquivalentTo(player);
+        }
+
+        // USING BOGUS TO CREATE A FAKE PLAYER OBJECT
+        [Fact(DisplayName = "GetByID Method when ID is invalid")]
+        [Trait("Ex.2", "PlayerServiceAutoMockerFixture Tests")]
+        public void GetByID_IdIsNotValid_ShouldThrowAnException()
+        {
+            // Arrange
+            Player player = _playerServiceTestsFixture.CreatePlayerUsingBogus();
+
+            var invalidId = player.ID - 100;
+
+            _playerServiceTestsFixture.Mocker.GetMock<IPlayerRepository>()
+                .Setup(pr => pr.GetByID(invalidId))
+                .Throws(new Exception("Id does not exists!"));
+
+            // Act
+            Action act = () => _subject.GetByID(invalidId);
+
+            // Assert
+            act.Should().Throw<Exception>()
+                .WithMessage("Id does not exists!");
+        }
+        #endregion
+
+        #region GetForLeague TESTS 
+        [Fact(DisplayName = "GetForLeague Method is successful")]
+        [Trait("Ex.2", "PlayerServiceAutoMockerFixture Tests")]
+        public void GetForLeague_IsSuccessful_ShouldReturnListOfPlayer()
+        {
+            // Arrange
+            var validLeagueID = 1;
+            var numberOfTeams = 2;
+            var numberOfPlayersPerTeam = 2;
+            var totalPlayers = numberOfTeams * numberOfPlayersPerTeam;
+
+            _playerServiceTestsFixture.Mocker.GetMock<ILeagueRepository>()
+                .Setup(lr => lr.IsValid(validLeagueID)).Returns(true);
+
+            var teams = _playerServiceTestsFixture.CreateListOfTeamsUsingBogus(numberOfTeams);
+            _playerServiceTestsFixture.Mocker.GetMock<ITeamRepository>()
+                .Setup(tr => tr.GetForLeague(validLeagueID)).Returns(teams);
+
+            var players = _playerServiceTestsFixture.CreateListOfPlayersUsingBogus(numberOfPlayersPerTeam);
+            _playerServiceTestsFixture.Mocker.GetMock<IPlayerRepository>()
+                .Setup(pr => pr.GetForTeam(It.IsAny<int>())).Returns(players);
+
+            // Act
+            var actual = _subject.GetForLeague(validLeagueID);
+
+            // Assert
+            actual.Should().NotBeNullOrEmpty();
+            actual.Should().HaveCount(totalPlayers);
+
+            _playerServiceTestsFixture.Mocker.GetMock<ITeamRepository>()
+                .Verify(tr => tr.GetForLeague(validLeagueID), Times.Once);
+
+            _playerServiceTestsFixture.Mocker.GetMock<IPlayerRepository>()
+                .Verify(pr => pr.GetForTeam(It.IsAny<int>()), Times.Exactly(numberOfTeams));
+        }
+
+        [Fact(DisplayName = "GetForLeague Method with Invalid League ID")]
+        [Trait("Ex.2", "PlayerServiceAutoMockerFixture Tests")]
+        public void GetForLeague_LeagueIDIsNotValid_ShouldReturnAnEmptyPlayerList()
+        {
+            // Arrange
+            var invalidLeagueID = 2;
+            _playerServiceTestsFixture.Mocker.GetMock<ILeagueRepository>()
+                .Setup(ml => ml.IsValid(invalidLeagueID)).Returns(false);
+
+            // Act
+            var actual = _subject.GetForLeague(invalidLeagueID);
+
+            // Assert
+            actual.Should().HaveCount(0);
+
+            _playerServiceTestsFixture.Mocker.GetMock<ITeamRepository>()
+                .Verify(tr => tr.GetForLeague(invalidLeagueID), Times.Never);
+
+            _playerServiceTestsFixture.Mocker.GetMock<IPlayerRepository>()
+                .Verify(pr => pr.GetForTeam(It.IsAny<int>()), Times.Never);
+        }
+        #endregion
+    }
+}
---
+++ Exercício 2 - Mock/MockingUnitTestsDemoApp.Tests/Services_AutoMockerFixture/PlayerServiceTestsFixture.cs
@@ -0,0 +1,129 @@
+﻿using AutoFixture;
+using Bogus;
+using Bogus.DataSets;
+using MockingUnitTestsDemoApp.Impl.Models;
+using MockingUnitTestsDemoApp.Impl.Repositories.Interfaces;
+using MockingUnitTestsDemoApp.Impl.Services;
+using Moq;
+using Moq.AutoMock;
+using Xunit;
+
+// https://hamidmosalla.com/2020/02/02/xunit-part-5-share-test-context-with-iclassfixture-and-icollectionfixture/
+// Using ICollectionFixture is useful because:
+// 
+
+namespace MockingUnitTestsDemoApp.Tests.Services_AutoMockerFixture
+{
+    [CollectionDefinition(nameof(PlayerServiceCollection))]
+    public class PlayerServiceCollection : ICollectionFixture<PlayerServiceTestsFixture>
+    { }
+
+    public class PlayerServiceTestsFixture : IDisposable
+    {
+        public PlayerService PlayerService;
+        public AutoMocker Mocker;
+
+        public PlayerService CreatePlayerService()
+        {
+            Mocker = new AutoMocker();
+            PlayerService = Mocker.CreateInstance<PlayerService>();
+
+            return PlayerService;
+        }
+
+        #region AutoFixture Methods
+        public Player CreatePlayerUsingAutoFixture()
+        {
+            return new Fixture().Create<Player>();
+        }
+
+        public List<Team> CreateListOfTeamsUsingAutoFixture(int numberOfTeams)
+        {
+            var teams = new List<Team>();
+
+            for (int i = 0; i < numberOfTeams; i++)
+            {
+                Team team = new Fixture().Create<Team>();
+                teams.Add(team);
+            }
+
+            return teams;
+        }
+
+        public List<Player> CreateListOfPlayersUsingAutoFixture(int numberOfPlayers)
+        {
+            var players = new List<Player>();
+
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                Player player = CreatePlayerUsingAutoFixture();
+                players.Add(player);
+            }
+
+            return players;
+        }
+        #endregion
+
+        #region Bogus Methods
+        public Player CreatePlayerUsingBogus()
+        {
+            Player player = new Faker<Player>("pt_BR")
+                .CustomInstantiator(f => new Player
+                {
+                    ID = f.Random.Int(1, 10),
+                    FirstName = f.Name.FirstName(Name.Gender.Male),
+                    LastName = f.Name.LastName(Name.Gender.Male),
+                    DateOfBirth = f.Date.Between(new DateTime(1980, 1, 1), new DateTime(2000, 1, 1)),
+                    TeamID = f.Random.Int(1, 5)
+
+                });
+
+            return player;
+        }
+        public List<Team> CreateListOfTeamsUsingBogus(int numberOfTeams)
+        {
+            var teams = new List<Team>();
+
+            for (int i = 0; i < numberOfTeams; i++)
+            {
+                Team team = new Faker<Team>("pt_BR")
+                    .CustomInstantiator(f => new Team
+                    {
+                        ID = f.Random.Int(1, numberOfTeams),
+                        Name = f.Name.FirstName(Name.Gender.Male),
+                        LeagueID = f.Random.Int(1, 5),
+                        FoundingDate = f.Date.Between(new DateTime(1980, 1, 1), new DateTime(2000, 1, 1))
+                    });
+                teams.Add(team);
+            }
+
+            return teams;
+        }
+
+        public List<Player> CreateListOfPlayersUsingBogus(int numberOfPlayers)
+        {
+            var players = new List<Player>();
+
+            for (int i = 0; i < numberOfPlayers; i++)
+            {
+                Player player = new Faker<Player>("pt_BR")
+                    .CustomInstantiator(f => new Player
+                    {
+                        ID = f.Random.Int(1, numberOfPlayers),
+                        FirstName = f.Name.FirstName(Name.Gender.Male),
+                        LastName = f.Name.LastName(Name.Gender.Male),
+                        DateOfBirth = f.Date.Between(new DateTime(1980, 1, 1), new DateTime(2000, 1, 1)),
+                        TeamID = f.Random.Int(1, 5)
+                    });
+                players.Add(player);
+            }
+
+            return players;
+        }
+        #endregion
+
+        public void Dispose()
+        {
+        }
+    }
+}
